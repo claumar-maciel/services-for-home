ea450176d9dc2922877e8987120d4f0c5e1e9f46
Claumar Maciel <claumar.maciel@gmail.com>
2022-08-29 19:56:07 -0300

    Finalizando o cadastro de cliente

--- app/Http/Controllers/Client/LoginController.php
+++ app/Http/Controllers/Client/LoginController.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers\Client;
 
+use App\Helpers\StringHelper;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Client\RegisterRequest;
+use App\Models\Contato;
+use App\Models\Endereco;
 use App\Models\Perfil;
+use App\Models\Usuario;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -21,6 +28,43 @@ class LoginController extends Controller
 
     public function register(RegisterRequest $request)
     {
-        dd(array_merge($request->all(), [ 'perfil_id' => Perfil::CLIENTE ]));
+        DB::transaction(function () use ($request) {
+            $request['celular'] = StringHelper::somenteNumeros($request->celular);
+            $request['telefone_residencial'] = StringHelper::somenteNumeros($request->telefone_residencial);
+            $dadosDoContato = array_merge(
+                $request->only('celular', 'telefone_residencial')
+            );
+            $contato = Contato::create($dadosDoContato);
+        
+            $request['cep'] = StringHelper::somenteNumeros($request->cep);
+            $dadosDoEndereco = array_merge(
+                $request->only('rua', 'numero', 'bairro', 'cidade', 'estado', 'cep', 'ponto_referencia', 'complemento')
+            );
+            $endereco = Endereco::create($dadosDoEndereco);
+    
+            $request['password'] = Hash::make($request->senha);
+            $request['cpf'] = StringHelper::somenteNumeros($request->cpf);
+            $dadosDoUsuario = array_merge(
+                $request->only('email', 'password', 'nome', 'cpf', 'username'), 
+                [ 
+                    'perfil_id' => Perfil::CLIENTE,
+                    'endereco_id' => $endereco->id, 
+                    'contato_id' => $contato->id, 
+                ]
+            );
+
+            $usuario = Usuario::create($dadosDoUsuario);
+
+            if ($usuario) {
+                return redirect('client.login')->with('success','cadastro realizado com sucesso!');
+            }
+        });
+
+        return back()->with('error','ocorreu um erro ao efetuar o cadastro!');
+    }
+
+    public function doLogin()
+    {
+        return view('client.login');
     }
 }
