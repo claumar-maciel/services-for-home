104579190ed1b8afb5083fb5e5f731040f73c765
Claumar Maciel <claumar.maciel@gmail.com>
2022-12-01 00:29:54 -0300

    deixa título de serviços maíusculo

--- resources/views/provider/services/index.blade.php
+++ resources/views/provider/services/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.provider.panel')
  
-@section('title', 'seus serviços')
+@section('title', 'Serviços')
  
 @section('content')
     <div class="d-flex justify-content-end w-100 mb-3">
